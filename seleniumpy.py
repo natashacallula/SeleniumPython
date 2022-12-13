fdb181750cdb8fb80997724d45ee28855af86372
natashacallula <natashacallula@gmail.com>
2022-12-13 21:46:48 +0700

    Update seleniumpy.py

--- seleniumpy.py
+++ seleniumpy.py
@@ -6,21 +6,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#Remove product from chart         
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-driver.find_element(By.ID,"user-name").send_keys("standard_user")   
-driver.find_element(By.ID,"password").send_keys("secret_sauce") 
-driver.find_element(By.ID,"login-button").click()
-time.sleep(1)
-driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
-time.sleep(1)
-driver.find_element(By.ID,"shopping_cart_container").click()    
-time.sleep(2)
-driver.find_element(By.ID,"remove-sauce-labs-bike-light").click()    
-time.sleep(1)
-driver.close()
-
 #Success login         
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
@@ -50,6 +35,21 @@ driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
 time.sleep(1)
 driver.find_element(By.ID,"shopping_cart_container").click()    
 time.sleep(2)
+driver.close()
+
+#Remove product from chart         
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.ID,"user-name").send_keys("standard_user")   
+driver.find_element(By.ID,"password").send_keys("secret_sauce") 
+driver.find_element(By.ID,"login-button").click()
+time.sleep(1)
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
+time.sleep(1)
+driver.find_element(By.ID,"shopping_cart_container").click()    
+time.sleep(2)
+driver.find_element(By.ID,"remove-sauce-labs-bike-light").click()    
+time.sleep(1)
 driver.close()
 
 #Test logout button        
